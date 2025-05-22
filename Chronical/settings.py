29ad14a5147ca03e30467ce2ce7d8a9e563756a6
vaibhav <vaibhavnagargoje2000@gmail.com>
2025-05-22 16:44:01 +0530

    design file creataed with final chart design

--- Chronical/settings.py
+++ Chronical/settings.py
@@ -87,6 +87,21 @@ DATABASES = {
 }
 
 
+# DATABASES = {  
+#     'default': {  
+#         'ENGINE': 'django.db.backends.mysql',  
+#         'NAME': 'chronical',  
+#         'USER': 'root',  
+#         'PASSWORD': '8806',  
+#         'HOST': '127.0.0.1',  
+#         'PORT': '3306',  
+#          'OPTIONS': {  
+#             'init_command': "SET sql_mode='STRICT_TRANS_TABLES'"  
+#         }          
+#     }  
+# }  
+
+
 # Password validation
 # https://docs.djangoproject.com/en/5.1/ref/settings/#auth-password-validators
 
