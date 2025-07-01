72b8d4a6898b6403ada9d8b6b124356d39cd5873
vaibhavnagargoje <vaibhavnagargoje2000@gmail.com>
2025-07-01 21:27:55 +0530

    create dropdown with dynamic slug with sate font size -

--- Chronical/settings.py
+++ Chronical/settings.py
@@ -161,33 +161,33 @@ WSGI_APPLICATION = 'Chronical.wsgi.application'
 # }
 
 
-DATABASES = {  
-    'default': {  
-        'ENGINE': os.getenv('DB_ENGINE', 'django.db.backends.mysql'),
-        'NAME': os.getenv('DB_NAME'),
-        'USER': os.getenv('DB_USER'),
-        'PASSWORD': os.getenv('DB_PASSWORD'),
-        'HOST': os.getenv('DB_HOST', '127.0.0.1'),
-        'PORT': os.getenv('DB_PORT', '3306'),
-        'OPTIONS': {
-            'init_command': "SET sql_mode='STRICT_TRANS_TABLES'"
-        }           
-    }  
-}  
-
 # DATABASES = {  
 #     'default': {  
-#         'ENGINE': 'django.db.backends.mysql',  
-#         'NAME': 'chronical',  
-#         'USER': 'root',  
-#         'PASSWORD': '8806',  
-#         'HOST': '127.0.0.1',  
-#         'PORT': '3306',  
-#          'OPTIONS': {  
-#             'init_command': "SET sql_mode='STRICT_TRANS_TABLES'"  
-#         }          
+#         'ENGINE': os.getenv('DB_ENGINE', 'django.db.backends.mysql'),
+#         'NAME': os.getenv('DB_NAME'),
+#         'USER': os.getenv('DB_USER'),
+#         'PASSWORD': os.getenv('DB_PASSWORD'),
+#         'HOST': os.getenv('DB_HOST', '127.0.0.1'),
+#         'PORT': os.getenv('DB_PORT', '3306'),
+#         'OPTIONS': {
+#             'init_command': "SET sql_mode='STRICT_TRANS_TABLES'"
+#         }           
 #     }  
 # }  
+
+DATABASES = {  
+    'default': {  
+        'ENGINE': 'django.db.backends.mysql',  
+        'NAME': 'chronical',  
+        'USER': 'root',  
+        'PASSWORD': '8806',  
+        'HOST': '127.0.0.1',  
+        'PORT': '3306',  
+         'OPTIONS': {  
+            'init_command': "SET sql_mode='STRICT_TRANS_TABLES'"  
+        }          
+    }  
+}  
 
 
 # Password validation
