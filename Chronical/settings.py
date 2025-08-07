d98c1aeb70644c4cc68fb0487d9cc983ee6107bd
vaibhavnagargoje <vaibhavnagargoje381@gmail.com>
2025-08-07 20:45:44 +0530

    crsf duplicate for the testing

--- Chronical/settings.py
+++ Chronical/settings.py
@@ -274,3 +274,12 @@ EMAIL_PORT = int(os.getenv('EMAIL_PORT', '587'))
 EMAIL_USE_TLS = os.getenv('EMAIL_USE_TLS', 'True').lower() == 'true'
 EMAIL_HOST_USER = os.getenv('EMAIL_HOST_USER')
 EMAIL_HOST_PASSWORD = os.getenv('EMAIL_HOST_PASSWORD')
+
+
+
+# CSRF_TRUSTED_ORIGINS = [
+#     'http://localhost:8000',
+#     'http://127.0.0.1:8000',
+#     # '238567e30c5d.ngrok-free.app',
+#     # 'https://238567e30c5d.ngrok-free.app',
+# ]
