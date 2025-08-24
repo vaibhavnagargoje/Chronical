41c5b0b842cb0ecd0e0f845bbd8b58a8a306835e
vaibhavnagargoje <vaibhavnagargoje2000@gmail.com>
2025-08-24 16:07:28 +0530

    fix setting with ssl error

--- Chronical/settings.py
+++ Chronical/settings.py
@@ -286,3 +286,6 @@ EMAIL_PORT = int(os.getenv('EMAIL_PORT', '587'))
 EMAIL_USE_TLS = os.getenv('EMAIL_USE_TLS', 'True').lower() == 'true'
 EMAIL_HOST_USER = os.getenv('EMAIL_HOST_USER')
 EMAIL_HOST_PASSWORD = os.getenv('EMAIL_HOST_PASSWORD')
+
+
+DATA_UPLOAD_MAX_NUMBER_FIELDS = 5000
