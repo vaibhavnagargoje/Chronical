9cd5047403fd54f346e34da05b43d9f14616d206
vaibhavnagargoje <vaibhavnagargoje2000@gmail.com>
2025-09-02 17:13:15 +0530

    db updated with .env

--- Chronical/settings.py
+++ Chronical/settings.py
@@ -160,17 +160,17 @@ if DEBUG:
 
     DATABASES = {  
         'default': {  
-            'ENGINE': 'django.db.backends.mysql',  
-            'NAME': 'chronical',  
-            'USER': 'root',  
-            'PASSWORD': '8806',  
-            'HOST': '127.0.0.1',  
-            'PORT': '3306',  
-            'OPTIONS': {  
-                'init_command': "SET sql_mode='STRICT_TRANS_TABLES'"  
-            }          
+            'ENGINE': os.getenv('DB_ENGINE', 'django.db.backends.mysql'),
+            'NAME': os.getenv('DB_NAME'),
+            'USER': os.getenv('DB_USER'),
+            'PASSWORD': os.getenv('DB_PASSWORD'),
+            'HOST': os.getenv('DB_HOST', '127.0.0.1'),
+            'PORT': os.getenv('DB_PORT', '3306'),
+            'OPTIONS': {
+                'init_command': "SET sql_mode='STRICT_TRANS_TABLES'"
+            }           
         }  
-    }  
+    }    
 
 #     DATABASES = {
 #     'default': {
